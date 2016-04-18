cc9a5a65e2d61ccc8350e28e6c250f15f2ffb2e6
Craig Dennis <craigmdennis@gmail.com>
2016-04-18 23:01:47 +0100

    Recursively process images

--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -8,14 +8,16 @@ var imagemin    = require('gulp-imagemin')
 var path        = require('path')
 
 var paths = {
-  src: path.join(config.root.src, config.tasks.images.src, '/**.{' + config.tasks.images.extensions + '}'),
+  src: path.join(config.root.src, config.tasks.images.src, '/**/*.{' + config.tasks.images.extensions + '}'),
   dest: path.join(config.root.dest, config.tasks.images.dest)
 }
 
 var imagesTask = function() {
   return gulp.src([paths.src, , '*!README.md'])
     .pipe(changed(paths.dest)) // Ignore unchanged files
-    .pipe(imagemin()) // Optimize
+    .pipe(imagemin({
+      progressive: true
+    })) // Optimize
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream())
 }
